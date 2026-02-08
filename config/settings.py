3b50211f9331cbbda452abaaf6d93666a5a29a48
AleksStaneva <aleksandra.h.staneva@gmail.com>
2026-02-08 11:44:18 +0200

    Add CRUD views and URL configuration for books and members

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books',
+    'members',
+    'quizzes',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
